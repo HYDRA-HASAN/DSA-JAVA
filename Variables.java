46ffbcf6140be3495d6c829c7e73c02c84e04533
Hassan Yaseen Siddiqui <hassanyaseen615@gmail.com>
2024-08-06 16:16:20 +0500

    1st

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myNumber = 88; // Declare an integer variable named myNumber
+        short myShort = 847; // Declare a short variable named myShort
+        long myLong = 9797; // Declare a long variable named myLong
+        double myDouble = 7.3243; // Declare a double variable named myDouble
+        float myFloat = 324.3f; // Declare a float variable named myFloat
+        char myChar = 'y'; // Declare a character variable named myChar
+        boolean myBoolean = false; // Declare a boolean variable named myBoolean
+        byte myByte = 127; // Declare a byte variable named myByte
+
+        System.out.println(myNumber); // Print the value of myNumber
+    }
+}
